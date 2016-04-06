b36f9f578f89b2b2eb615be68f474b8c75428edd
elmayis <marhtrev@gmail.com>
2016-04-06 17:42:39 -0500

    Added SerialPort object

--- SlaSerialComm/SlaSerialCommMainForm.cs
+++ SlaSerialComm/SlaSerialCommMainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,29 @@ using System.Windows.Forms;
 
 namespace SlaSerialComm
 {
-    public partial class SlaSerialCommMainForm : Form
-    {
-        public SlaSerialCommMainForm()
-        {
-            InitializeComponent();
-        }
-    }
+   public partial class SlaSerialCommMainForm : Form
+   {
+      SerialPort ComPort = new SerialPort();
+
+      public SlaSerialCommMainForm()
+      {
+         InitializeComponent();
+      }
+
+      private void btnSerialPorts_Click(object sender, EventArgs e)
+      {
+         string[] ArrayComPortsNames = null;
+         int index = -1;
+         string ComPortName = null;
+
+         ArrayComPortsNames = SerialPort.GetPortNames();
+         do
+         {
+            index += 1;
+            rtbIncomingData.Text += ArrayComPortsNames[index] + "\r\n";
+         }
+         while (!((ArrayComPortsNames[index] == ComPortName) ||
+                             (index == ArrayComPortsNames.GetUpperBound(0))));
+      }
+   }
 }
